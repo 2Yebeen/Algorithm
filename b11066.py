93059b586e1c54641a09fa35c2d71b54e8db48e3
yebeen <33bani@gmail.com>
2022-03-23 14:55:45 +0900

    .✍ b : 백준, p : 프로그래머스, 이름 수정 및 4주차 백준 알고리즘 스터디

---
+++ b11066.py
@@ -0,0 +1,19 @@
+"""
+[백준/11066] 파일 합치기
+https://www.acmicpc.net/problem/11066
+https://bini-079.tistory.com/108
+
+    input
+2
+4
+40 30 30 50
+15
+1 21 3 4 5 35 5 4 3 5 98 21 14 17 32
+    output
+300
+864
+"""
+import sys
+input = sys.stdin.readline
+
+TC = int(input())
