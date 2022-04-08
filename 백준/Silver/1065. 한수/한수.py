1ebcc71dfd1516ea5bfef130461e7c93579e79c4
33bini <68188768+33bini@users.noreply.github.com>
2022-04-09 01:15:44 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,10 +1,9 @@
 input = __import__('sys').stdin.readline
-
 N = int(input())
 if N < 100:
     print(N)
 else:
     cnt = 99
     for i in range(100, N+1):
-        if (i//100) - (i%100//10) == (i%100//10) - (i%100%10): cnt += 1
+        if (i%100//10)*2 == (i//100) + (i%100%10): cnt += 1
     print(cnt)
