5c2b124c81f3bc23f00d54db2518d0326691b431
33bini <68188768+33bini@users.noreply.github.com>
2022-04-09 01:06:42 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,18 +1,11 @@
 input = __import__('sys').stdin.readline
 
-def Hansoo(x):
-    if int(x[0]) - int(x[1]) == int(x[1]) - int(x[2]):
-        return 1
-    elif int(x) == 1000:
-        return 0
-    else:
-        return 0
-
 N = int(input())
 if N < 100:
     print(N)
 else:
     cnt = 99
     for i in range(100, N+1):
-        cnt += Hansoo(str(i))
+        x = str(i)
+        if int(x[0]) - int(x[1]) == int(x[1]) - int(x[2]): cnt += 1
     print(cnt)
