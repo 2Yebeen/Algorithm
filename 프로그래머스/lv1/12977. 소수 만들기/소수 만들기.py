f35fa9eb0869f459f52b0c352e7b653b1fa538bf
Ye-Been Lee <68188768+yeblee@users.noreply.github.com>
2022-09-03 18:33:11 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,17 @@
+from itertools import combinations, permutations
+def is_prime_number(x):
+    # 2부터 (x - 1)까지의 모든 수를 확인하며
+    for i in range(2, x):
+        # x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+def solution(nums):
+    answer = 0
+    combi = list(combinations(nums, 3))
+    
+    for i in combi:
+        if is_prime_number(sum(i)):
+            answer += 1
+    return answer
