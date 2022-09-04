9a6343bc38b0764500aa07b05bdfa384e7123a8d
Ye-Been Lee <68188768+yeblee@users.noreply.github.com>
2022-09-04 15:12:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 82.13 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,6 @@
 def solution(arr):
     ans = [arr[0]]
-    a = 0
     for i in range(len(arr)):
-        if ans[a] != arr[i]:
+        if ans[-1] != arr[i]:
             ans.append(arr[i])
-            a += 1
     return ans
