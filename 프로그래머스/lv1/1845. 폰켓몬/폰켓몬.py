864ae2b7086b51be36829409d8cbf5b4e9186f0a
Ye-Been Lee <68188768+yeblee@users.noreply.github.com>
2022-10-28 23:49:44 +0900

    [level 1] Title: 폰켓몬, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    n = len(nums) / 2
-    new_nums = list(set(nums))
-    if n >= len(new_nums): 
-        return len(new_nums)
-    else:
-        return n
+    return min(len(nums) / 2, len(list(set(nums))))
