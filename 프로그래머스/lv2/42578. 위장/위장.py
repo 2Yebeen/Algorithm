1b015f2f1921f7be316c6263225ef3ffdb76ba76
Ye-Been Lee <68188768+yeblee@users.noreply.github.com>
2022-11-28 14:40:53 +0900

    [level 2] Title: 위장, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
-from itertools import product
 
 def solution(clothes):
     answer = 1
-    keys = [ k for v, k in clothes]
-    counts = [keys.count(key) for key in set(keys)]
-    print(keys, counts)
-    for c in counts:
-        answer *= c + 1
-    # 모든 경우에서 의상을 하나도 고르지 않는 경우 제외
+    clothes_dict = defaultdict(list)
+
+    for value, key in clothes:
+        clothes_dict[key].append(value)
+
+    for i in clothes_dict.keys():
+        answer *= len(clothes_dict[i]) + 1
     return answer - 1
