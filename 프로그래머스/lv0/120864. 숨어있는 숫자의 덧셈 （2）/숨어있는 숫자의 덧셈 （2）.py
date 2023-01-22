f765bb515cfb56fd981b20afcdcc12fe6b2f7522
Ye-Been Lee <yebeen.dev@gmail.com>
2023-01-22 15:13:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,5 +1,4 @@
 import re
 
 def solution(my_string):
-    numbers = list(map(int, re.findall(r'\d+', my_string)))
-    return sum(numbers)
+    return sum(list(map(int, re.findall(r'\d+', my_string))))
